b533ae6222c5eacffcde9776505f811de40ee221
JuanDiegoCC02 <dcorella2324@gmail.com>
2025-12-29 17:30:16 -0600

    1part style home explain app

--- src/components/ExplainAppHome.jsx
+++ src/components/ExplainAppHome.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../Styles/ExplainAppHome.css"
 import formImgRecipes from '../images/formRecipes.png'
 import listImgRecipes from '../images/listGetRecipes.png'
 
@@ -6,18 +7,18 @@ import listImgRecipes from '../images/listGetRecipes.png'
 function ExplainAppHome() {
   return (
     <div>
-        <div><h1>Explanation of the Todo-List of Recipes</h1></div>
+        <div className='containerTitleHome'><h1 className='TitleHome'>Explanation of the Todo-List of Recipes</h1></div>
 
-  <div>
-        <div>
-            <h3>Recipe creation form</h3>
-            <strong>Description</strong>
-            <div><img src={formImgRecipes} alt="" /></div>
+  <div className='containerAllCardsHome'>
+        <div className='containerCardHome'>
+            <h3 className='TitleExplainApp'>Recipe creation form</h3>
+            <strong className='DescriptionApp'>Description</strong>
+            <div className='containerImgExplain'><img className='ImgExplain' src={formImgRecipes} alt="" /></div>
         </div><br /><br />
-          <div>
-            <h3>Get the Recipe list</h3>
-            <strong>Desription</strong>
-            <div><img src= {listImgRecipes} alt="" /></div>
+          <div className='containerCardHome'>
+            <h3 className='TitleExplainApp'>Get the Recipe list</h3>
+            <strong className='DescriptionApp'>Desription</strong>
+            <div className='containerImgExplain'><img className='ImgExplain' src= {listImgRecipes} alt="" /></div>
         </div>
   </div>
 
