24c56cd6ec1f11c972fd3bed6d7d8a132a358d10
JuanDiegoCC02 <dcorella2324@gmail.com>
2026-02-02 13:10:38 -0600

    Updating newStructure & newInfo HomeCookingRecipes

--- src/components/ExplainAppHome.jsx
+++ src/components/ExplainAppHome.jsx
@@ -15,22 +15,33 @@ function ExplainAppHome() {
   <div className='containerAllCardsHome'>
         <div className='containerCardHome'>
             <h3 className='TitleExplainApp'>Recipe Creation Form</h3>
-            <strong className='DescriptionApp'>Description</strong>
+            <strong className='DescriptionApp'>
+               In this form you will be able to create recipes, entering the Recipe Name,
+               a Recipe Description and the Ingredients you will use.
+               </strong>
             <div className='containerImgExplain'><img className='ImgExplain' src={formImg} alt="" /></div>
         </div>
          <div className='containerCardHome'>
             <h3 className='TitleExplainApp'> Recipe Counter </h3>
-            <strong className='DescriptionApp'>Description</strong>
+            <strong className='DescriptionApp'>
+              In the ListCookingRecipe area you will find a counter which will keep track of the 
+              recipes you complete.
+            </strong>
             <div className='containerImgExplain'><img className='ImgExplain' src={listImg} alt="" /></div>
         </div>
          <div className='containerCardHome'>
             <h3 className='TitleExplainApp'>Get Recipe List</h3>
-            <strong className='DescriptionApp'>Description</strong>
+            <strong className='DescriptionApp'>
+              In this same area, the record of the recipes you have generated in the form will be displayed.
+            </strong>
             <div className='containerImgExplain'><img className='ImgExplain' src={recipeImg} alt="" /></div>
         </div>
           <div className='containerCardHome'>
             <h3 className='TitleExplainApp'>Add steps to the Recipe</h3>
-            <strong className='DescriptionApp'>Desription</strong>
+            <strong className='DescriptionApp'>
+              At the bottom of the recipe card you will find an option titled "add steps".
+              Here you can open a form to add all the steps of the recipe you carry out.
+            </strong>
             <div className='containerImgExplain'><img className='ImgExplain' src= {stepImg} alt="" /></div>
         </div>
   </div>
--- src/components/HomeCookingRecipes.jsx
+++ src/components/HomeCookingRecipes.jsx
@@ -4,8 +4,7 @@ import ExplainAppHome from './ExplainAppHome'
 function HomeCookingRecipes() {
   return (
     <div>
-        <h1>Cooking Recipes</h1>
-
+      
       <ExplainAppHome/>
 
     </div>
